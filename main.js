8a4e6932f0a285e845163f90021c6cf8b0b5f57b
kookontheloose <141165990+kookontheloose@users.noreply.github.com>
2024-06-18 15:11:17 -0700

    Update main.js
    cleaned up comments

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const lapRecordDiv = document.getElementById("laps");
+// ^ the above is the construct I was mentioning. Now you only ever need to use lapRecordDiv below
+
 let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let displayTime = document.getElementById("displayTime");
+let [h, m, s, ms] = [0, 0, 0, 0];
+const displayTime = document.getElementById("displayTime");
+// ^ good - but you're never reassigning it so it can be const
 let timer = null;
 
 function stopwatch() {
@@ -17,26 +22,21 @@ function stopwatch() {
     }
   }
 
-  let h = hours < 10 ? "0" + hours : hours;
-  let m = minutes < 10 ? "0" + minutes : minutes;
-  let s = seconds < 10 ? "0" + seconds : seconds;
-
-  let ms = Math.floor(milliseconds / 10);
-  ms < 10 ? "0" + milliseconds : milliseconds;
-  //line 24/25 fixed the problem of the extra zero
-  //what i originally had: let ms = milliseconds < 100 ? "0" + milliseconds : milliseconds;
+  h = hours < 10 ? "0" + hours : hours;
+  m = minutes < 10 ? "0" + minutes : minutes;
+  s = seconds < 10 ? "0" + seconds : seconds;
+  ms = Math.floor(milliseconds / 10);
 
   displayTime.innerHTML = h + ":" + m + ":" + s + ":" + ms;
 }
 
 function lap() {
-  let laps = null;
-  let lapNow = `<div id="lap">${hours} : ${minutes} : ${seconds} : ${milliseconds}</div>`;
-  laps = document.getElementById("lapRecord").innerHTML + lapNow;
-  document.getElementById("lapRecord").innerHTML = laps;
+  let lapNow = `<div>${h} : ${m} : ${s} : ${ms}</div>`;
+  lapRecordDiv.innerHTML += lapNow;
 }
 
 function watchStart() {
+  console.log("clicked");
   if (timer !== null) {
     clearInterval(timer);
   }
@@ -46,14 +46,10 @@ function watchStart() {
 function watchStop() {
   clearInterval(timer);
 }
+
 function watchReset() {
   clearInterval(timer);
   [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
   displayTime.innerHTML = "00:00:00:00";
+  lapRecordDiv.innerHTML = "";
 }
-
-//document.getElementById("lapRecord").style.display = "none";
-
-//get rid of extra 0 on milliseconds - within lap
-
-// add zero infront of single digit seconds
